ac530a775db7c7565e1baadb0583ca07f04e7d19
Mariano Simone <marianosimone@thumbtack.com>
2017-04-25 15:52:17 -0400

    Bind views with Data Binding instead of Butterknife

--- NerdMart/app/src/main/java/com/bignerdranch/android/nerdmart/LoginFragment.java
+++ NerdMart/app/src/main/java/com/bignerdranch/android/nerdmart/LoginFragment.java
@@ -1,26 +1,18 @@
 package com.bignerdranch.android.nerdmart;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import com.bignerdranch.android.nerdmart.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends NerdMartAbstractFragment {
-
-    @Bind(R.id.fragment_login_username)
-    EditText mUsernameEditText;
-
-    @Bind(R.id.fragment_login_password)
-    EditText mPasswordEditText;
 
     @Nullable
     @Override
@@ -30,28 +22,34 @@ public class LoginFragment extends NerdMartAbstractFragment {
             final @Nullable Bundle savedInstanceState) {
 
         super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
-        ButterKnife.bind(this, view);
-        return view;
-    }
-
-    @OnClick(R.id.fragment_login_login_button)
-    public void handleLoginClick() {
-        String username = mUsernameEditText.getText().toString();
-        String password = mPasswordEditText.getText().toString();
-        addSubscription(
-                mNerdMartServiceManager
-                        .authenticate(username, password)
-                        .compose(loadingTransformer())
-                        .subscribe(authenticated -> {
+        final FragmentLoginBinding fragmentLoginBinding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_login,
+                container,
+                false);
+        fragmentLoginBinding.setLoginButtonClickListener(v -> {
+            String username = fragmentLoginBinding.fragmentLoginUsername
+                    .getText().toString();
+            String password = fragmentLoginBinding.fragmentLoginPassword
+                    .getText().toString();
+            addSubscription(mNerdMartServiceManager
+                    .authenticate(username, password)
+                    .compose(loadingTransformer())
+                    .subscribe(authenticated -> {
+                        if (!authenticated) {
                             Toast.makeText(getContext(),
-                                    R.string.auth_success_toast,
+                                    R.string.auth_failure_toast,
                                     Toast.LENGTH_SHORT).show();
-                            final Intent intent = ProductsActivity.newIntent(getContext());
-                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
-                                    Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(intent);
-                            getActivity().finish();
-                        }));
+                            return;
+                        }
+                        Toast.makeText(getContext(),
+                                R.string.auth_success_toast,
+                                Toast.LENGTH_SHORT).show();
+                        Intent intent = ProductsActivity
+                                .newIntent(getContext());
+                        getActivity().finish();
+                        startActivity(intent);
+                    }));
+        });
+        return fragmentLoginBinding.getRoot();
     }
 }
--- NerdMart/app/src/main/java/com/bignerdranch/android/nerdmart/ProductsFragment.java
+++ NerdMart/app/src/main/java/com/bignerdranch/android/nerdmart/ProductsFragment.java
@@ -1,15 +1,26 @@
 package com.bignerdranch.android.nerdmart;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bignerdranch.android.nerdmart.databinding.FragmentProductsBinding;
+import com.bignerdranch.android.nerdmartservice.service.payload.Product;
+
+import java.util.Collections;
+
 import timber.log.Timber;
 
 public class ProductsFragment extends NerdMartAbstractFragment {
+
+    private FragmentProductsBinding mFragmentProductsBinding;
+
+    private ProductRecyclerViewAdapter mAdapter;
 
     @Override
     public View onCreateView(
@@ -17,10 +28,24 @@ public class ProductsFragment extends NerdMartAbstractFragment {
             final @Nullable ViewGroup container,
             final @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        final View view =
-                inflater.inflate(R.layout.fragment_products, container, false);
+        mFragmentProductsBinding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_products, container, false);
+        final ProductRecyclerViewAdapter.AddProductClickEvent addProductClickEvent
+                = this::postProductToCart;
+        mAdapter = new ProductRecyclerViewAdapter(
+                Collections.emptyList(), getContext(), addProductClickEvent);
+        setupAdapter();
         updateUI();
-        return view;
+        return mFragmentProductsBinding.getRoot();
+    }
+
+    private void setupAdapter() {
+        final LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(getContext());
+        mFragmentProductsBinding.fragmentProductsRecyclerView
+                .setLayoutManager(linearLayoutManager);
+        mFragmentProductsBinding.fragmentProductsRecyclerView
+                .setAdapter(mAdapter);
     }
 
     private void updateUI() {
@@ -29,6 +54,12 @@ public class ProductsFragment extends NerdMartAbstractFragment {
                         .getProducts()
                         .toList()
                         .compose(loadingTransformer())
-                        .subscribe(products -> Timber.i("received products: " + products)));
+                        .subscribe(products -> {
+                            mAdapter.setProducts(products);
+                            mAdapter.notifyDataSetChanged();
+                        }));
+    }
+
+    private void postProductToCart(final @NonNull Product product){
     }
 }
