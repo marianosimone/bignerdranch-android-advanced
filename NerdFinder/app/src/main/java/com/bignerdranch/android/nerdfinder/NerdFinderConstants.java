0dcc0c64adf75fb76008f826b5f7b487b90519e9
Mariano Simone <marianosimone@thumbtack.com>
2017-04-28 10:46:41 -0400

    Remove injection from NerdFinder

--- NerdFinder/app/src/main/java/com/bignerdranch/android/nerdfinder/NerdFinderConstants.java
+++
@@ -1,6 +0,0 @@
-package com.bignerdranch.android.nerdfinder;
-
-public interface NerdFinderConstants {
-    String INJECTOR_SERVICE
-            = "com.bignerdranch.nerdmart.inject.Injector";
-}
--- NerdFinder/app/src/main/java/com/bignerdranch/android/nerdfinder/inject/NerdFinderGraph.java
+++
@@ -1,7 +0,0 @@
-package com.bignerdranch.android.nerdfinder.inject;
-
-public interface NerdFinderGraph {
-
-//    void inject(final AbstractFragment fragment);
-//    void inject(final NerdMartAbstractActivity activity);
-}
